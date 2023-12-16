cf6d76ed4687f76aab469e11d01a376aded58668
kyungmun <kyungmun@gmail.com>
2023-12-17 00:22:29 +0900

    progress templete apply

--- progress/chage.go
+++ progress/chage.go
@@ -17,17 +17,20 @@ func worker(job *Job, wg *sync.WaitGroup) {
 
 	//progress := job.progress
 	job.progress.Set("prefix", "["+job.name+"]")
-	job.progress.SetWidth(80)
+	job.progress.SetTemplateString(`{{string . "prefix"}}{{bar . "[" "=" ">" "-" "]"}} {{percent . }} {{string . "suffix"}}`)
+	//job.progress.SetTemplateString("template", `{{string . "prefix"}}{{counters . }} {{bar . "[" "=" ">" "-" "]"}} {{percent . }} {{string . "suffix"}}`)
+
+	//job.progress.SetWidth(80)
 	// 여기에서 실제 작업 수행
 	for i := 0; i < 100; i += 1 {
 		if job.name == "Job1" {
-			time.Sleep(100 * time.Millisecond) // 실제 작업 대체 필요
+			time.Sleep(10 * time.Millisecond) // 실제 작업 대체 필요
 		}
 		if job.name == "Job2" {
-			time.Sleep(300 * time.Millisecond) // 실제 작업 대체 필요
-		} else {
-			time.Sleep(100 * time.Millisecond) // 실제 작업 대체 필요
+			time.Sleep(30 * time.Millisecond) // 실제 작업 대체 필요
 		}
+
+		time.Sleep(100 * time.Millisecond) // 실제 작업 대체 필요
 		job.progress.Increment()
 	}
 	job.progress.Finish()
@@ -43,40 +46,27 @@ func main() {
 
 	// 진행률 표시 준비
 	first := pb.New(100)
-	first.SetWidth(80)
+	first.SetMaxWidth(100)
 	second := pb.New(100)
-	second.SetWidth(80)
+	second.SetMaxWidth(100)
 	third := pb.New(100)
-	third.SetWidth(80)
+	third.SetMaxWidth(100)
 	jobs[0].progress = first
 	jobs[1].progress = second
 	jobs[2].progress = third
-	// bar.SetWidth(80)
-	// bar.Set("template", `{{string . "prefix"}}{{counters . }} {{bar . "[" "=" ">" "-" "]"}} {{percent . }} {{string . "suffix"}}`)
-	// bar.Set("prefix", "Overall Progress ")
-	// bar.SetCurrent(0)
 
-	pool, err := pb.StartPool(first, second, third)
-	if err != nil {
-		panic(err)
-	}
-	// pool := pb.NewPool()
-	// pool.Add(first)
-	// pool.Add(second)
-	// pool.Add(third)
-	// pool.Start()
+	// pool, err := pb.StartPool(first, second, third)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	pool := pb.NewPool()
+	pool.Add(first)
+	pool.Add(second)
+	pool.Add(third)
+	pool.Start()
 
 	for i := range jobs {
-
-		// bar := pb.StartNew(100)
-		//jobs[i].progress.SetWidth(80)
-		// bar.Set("template", `{{string . "prefix"}}{{counters . }} {{bar . "[" "=" ">" "-" "]"}} {{percent . }} {{string . "suffix"}}`)
-		// //bar.Set("prefix", "Overall Progress ")
-		//jobs[i].progress.SetCurrent(0)
-		// jobs[i].progress = bar
-
-		// pb.NewPool(bar)
-
 		wg.Add(1)
 		go worker(&jobs[i], &wg)
 	}
